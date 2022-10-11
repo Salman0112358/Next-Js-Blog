ca99f225a0cb55c9913795cb0bac9ee4de684c52
Salman0112358 <Salman0112358@outlook.com>
2022-10-11 22:56:06 +0100

    planning initial layout of home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,38 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Head from "next/head";
 
+const posts = [
+  { title: "React Testing", excerpt: "Learn React testing" },
+
+  {
+    title: "React with Tailwind",
+    excerpt: "Learn react with tailwind",
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>hello every body</h1>
-    </div>
-  )
-}
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS Blog</title>
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div key={index}>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1"></div>
+        <div className="lg:sticky relative top-8">
+          
 
-export default Home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
