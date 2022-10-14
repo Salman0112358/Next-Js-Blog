3045b6fb9cedfe0b40e2a2d6a407a5addc6bf8a4
Salman0112358 <Salman0112358@outlook.com>
2022-10-14 01:47:12 +0100

    adding card tailwing css for post card component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,10 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import {getPosts} from '../services/index'
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React testing" },
+export default function Home ({posts}) {
 
-  {
-    title: "React with Tailwind",
-    excerpt: "Learn react with tailwind",
-  },
-];
-
-const Home = () => {
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,7 +12,7 @@ const Home = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (<PostCard post={post} key={post.title}/>))}
+          {posts.map((post) => (<PostCard post={post.node} key={post.node.title}/>))}
         </div>
         <div className="lg:col-span-4 col-span-1"></div>
           <div className="lg:sticky relative top-8">
@@ -30,4 +24,11 @@ const Home = () => {
   );
 };
 
-export default Home;
+
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+
+  return {
+    props : {posts}
+  }
+}
--- services/index.js
+++ services/index.js
@@ -1,33 +1,44 @@
+
 import { request, gql } from "graphql-request"; //allows for graph ql querying
 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+console.log("hello")
+
 export const getPosts = async () => {
-  const query = gql`
-    query MyQuery {
-      postsConnection {
-        edges {
-          node {
-            author {
-              bio
-              id
-              name
-              photo {
+    const query = gql`
+      query MyQuery {
+        postsConnection {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
                 url
               }
-            }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
+              categories {
+                name
+                slug
+              }
             }
           }
         }
       }
-      categories {
-        name
-        slug
-      }
-    }
-  `;
-};
+    `;
+  
+    const result = await request(graphqlAPI, query);
+  
+    return result.postsConnection.edges;
+  };
+  
