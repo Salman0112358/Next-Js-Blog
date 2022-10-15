0645b75c1b66f55c6cd04609b2f6cda70dd0bb3d
Salman0112358 <Salman0112358@outlook.com>
2022-10-15 23:44:55 +0100

    useEffect with async anonymous arrow function to retrieve a single load of all the categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
+import { getCategories } from '../services';
 
 const Categories = () => {
+
+  const [cateogires, setCateogires] = useState([])
+
+  useEffect(() => {
+
+    async () => {
+      setCateogires(await getCategories())
+    }
+    
+
+  }, [])
+
   return (
     <div>Categories</div>
   )
