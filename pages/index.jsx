c6a292085d5727a07f1e62033d34c7f6f08c6df6
Salman0112358 <Salman0112358@outlook.com>
2022-10-15 23:38:09 +0100

    Added a component to show recent/related post

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,11 +16,12 @@ export default function Home ({posts}) {
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (<PostCard post={post.node} key={post.node.title}/>))}
         </div>
-        <div className="lg:col-span-4 col-span-1"></div>
+        <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget/>
             <Categories/>
           </div>
+        </div>
       </div>
     </div>
   );
