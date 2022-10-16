4e13a9f0258fd4a803be533127f5d6387d710086
Salman0112358 <Salman0112358@outlook.com>
2022-10-16 21:44:20 +0100

    styling changes on author profile picture

--- components/Author.jsx
+++ components/Author.jsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 const Author = ({author}) => {
   return (
-    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-zinc-900 bg-opacity-80'>
+    <div className='text-center mt-20 mb-8 p-24 relative rounded-lg bg-zinc-900 bg-opacity-80'>
       <div className='absolute left-0 right-0 -top-14'>
 
       <Image
         unoptimized
-        height="100px"
-        width="100px"
+        height="150px"
+        width="150px"
         className='align-middle rounded-full'
         src={author.photo.url}
       />
